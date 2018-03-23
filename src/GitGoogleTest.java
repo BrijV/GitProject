1949ba83ccea03587302c6857472174ba0e0ce35
brijv <brij0924@gmail.com>
2018-03-22 22:51:06 -0400

    initial project structure

---
+++ src/GitGoogleTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+public class GitGoogleTest {
+
+	public static void main(String[] args) {
+
+		String Browsers = "Firefox";
+		String title = "book - Google Search";
+		WebDriver driver=null;
+
+		if (Browsers.equalsIgnoreCase("Firefox")) {
+			driver = new FirefoxDriver();
+		} else if(Browsers.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver","C:\\WebDriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+		driver.get("http://google.com");
+		driver.findElement(By.xpath(constants.TEXT_BOX_XPAHT)).sendKeys("book",
+				Keys.ENTER);
+		Assert.assertEquals(driver.getTitle(), title);
+		System.out.println(driver.findElement(
+				By.xpath(constants.CLICK_FIRST_LINK)).getText());
+		driver.quit();
+
+	}
+
+}
---
+++ src/constants.java
@@ -0,0 +1,7 @@
+
+public class constants {
+
+	public static final String TEXT_BOX_XPAHT = "//INPUT[@id='lst-ib']";
+	public static final String CLICK_FIRST_LINK = "//A[@href='https://en.wikipedia.org/wiki/Book'][text()='Book - Wikipedia']";
+
+}
